8e34056950d8927f3aa52951a309b2784f6a87ba
mohamed <mezoattalla123@gmail.com>
2024-03-02 13:49:36 +0200

    splash view body done

--- lib/features/splash/presentation/view_models/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/view_models/views/widgets/splash_view_body.dart
@@ -1,10 +1,31 @@
+import 'package:easy_splash_screen/easy_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:my_bookly/constants.dart';
+import 'package:my_bookly/core/utils/assets.dart';
 
 class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column();
+    return EasySplashScreen(
+      logoWidth: 200,
+      logo: Image.asset(
+        AssetsData.logo,
+      ),
+      title: const Text(
+        "Read free books",
+        style: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      backgroundColor: kPrimaryColor,
+      showLoader: true,
+      loaderColor: Colors.white,
+      loadingText: const Text("Loading..."),
+      navigator: Container(),
+      durationInSeconds: 15,
+    );
   }
 }
