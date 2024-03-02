ed51ebd9c50c05124957c3f230cc93acb2ce5458
mohamed <mezoattalla123@gmail.com>
2024-03-02 08:08:53 +0200

    change theme

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
-      home:  SplashView(),
+
+    return  GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home:  const SplashView(),
     );
   }
 }
