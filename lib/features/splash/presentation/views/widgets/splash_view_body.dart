7b49f414175b9d02a219b6a9e3949c7edb1baeef
mohamed <mezoattalla123@gmail.com>
2024-03-03 18:00:48 +0200

    navigator to home view

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:my_bookly/features/home/presentation/views/home_view.dart';
 import 'slider_image_animation.dart';
 
 class SplashViewBody extends StatefulWidget {
@@ -16,11 +19,16 @@ class _SplashViewBodyState extends State<SplashViewBody>
   @override
   void initState() {
     initSlidingAnimation();
-
     super.initState();
+    Future.delayed(
+        const Duration(
+          seconds: 2,
+        ), () {
+      Get.to(() => const HomeView(),
+          transition: Transition.fade,
+          duration: const Duration(milliseconds: 250));
+    });
   }
-
-
 
   @override
   void dispose() {
@@ -34,7 +42,9 @@ class _SplashViewBodyState extends State<SplashViewBody>
       mainAxisAlignment: MainAxisAlignment.end,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        SliderImageAnimation(slidingAnimation: slidingAnimation,),
+        SliderImageAnimation(
+          slidingAnimation: slidingAnimation,
+        ),
         const Center(
           child: Padding(
             padding: EdgeInsets.only(bottom: 16.0),
@@ -50,6 +60,7 @@ class _SplashViewBodyState extends State<SplashViewBody>
       ],
     );
   }
+
   void initSlidingAnimation() {
     animationController = AnimationController(
       vsync: this,
