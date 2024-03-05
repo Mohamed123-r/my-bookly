aa5697d857a0dc6ef1094df1e9e1815cd7d40e29
mohamed <mezoattalla123@gmail.com>
2024-03-05 18:40:10 +0200

    done create app Styles

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,8 +1,30 @@
 import 'package:flutter/material.dart';
 
 abstract class Styles {
+  static const textStyle16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const textStyle18 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+  );
+
   static const textStyle20 = TextStyle(
-    fontSize: 22,
+    fontSize: 20,
     fontWeight: FontWeight.w600,
+  );
+
+  static const textStyle24 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.normal,
+    fontFamily: "Benne-Regular",
+  );
+
+  static const textStyle32 = TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeight.normal,
+    fontFamily: "Benne-Regular",
   );
 }
