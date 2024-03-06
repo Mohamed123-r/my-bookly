1cf601a96f7f710afa9e27d4778333fd50bf5000
mohamed <mezoattalla123@gmail.com>
2024-03-06 20:54:01 +0200

    create best seller list view and custom scroll view

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_bookly/core/utils/styles.dart';
 import 'package:my_bookly/features/home/presentation/views/widgets/custom_app_bar.dart';
-import 'best_seller_list_view_item.dart';
+import 'best_seller_list_view.dart';
 import 'books_list_view.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -10,25 +10,38 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const SafeArea(
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            CustomAppBar(),
-            BooksListView(),
-            Text(
-              "Best Seller",
-              style: Styles.textStyle20
+      child: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.0),
+                  child: CustomAppBar(),
+                ),
+                BooksListView(),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Text(
+                    "Best Seller",
+                    style: Styles.textStyle20,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+              ],
             ),
-            SizedBox(
-              height: 20,
+          ),
+          SliverFillRemaining(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              child: BestSellerListView(),
             ),
-            BestSellerListViewItem(),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
 }
-
