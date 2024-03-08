b3e338f7139f8c708abc7f3e8db3f87e3e623fcf
mohamed <mezoattalla123@gmail.com>
2024-03-08 19:50:55 +0200

    push to book details view

--- lib/features/home/presentation/views/widgets/best_seller_list_view.dart
+++ lib/features/home/presentation/views/widgets/best_seller_list_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_bookly/features/home/presentation/views/book_details_view.dart';
 import 'custom_best_seller_item.dart';
 
 class BestSellerListView extends StatelessWidget {
@@ -8,9 +9,20 @@ class BestSellerListView extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.separated(
       physics: const NeverScrollableScrollPhysics(),
-      itemCount:20,
+      itemCount: 20,
       itemBuilder: (context, index) {
-        return const BestSellerListViewItem();
+        return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (no) {
+                    return const BookDetailsView();
+                  },
+                ),
+              );
+            },
+            child: const BestSellerListViewItem());
       },
       separatorBuilder: (BuildContext context, int index) {
         return const SizedBox(
