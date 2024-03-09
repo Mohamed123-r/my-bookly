f18844a77008b7fb171122c9d65ffb37bc4b2bd0
mohamed <mezoattalla123@gmail.com>
2024-03-09 16:45:38 +0200

    create custom_book_details_appBar.dart

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 
+import 'custom_book_details_appBar.dart';
+
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Column(
+        children: [
+          CustomBookDetailsAppBar(),
+        ],
+      ),
+    );
   }
 }
+
+
