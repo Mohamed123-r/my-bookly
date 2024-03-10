5499d2d445c076aaff0c7842cfc7116f726b771b
mohamed <mezoattalla123@gmail.com>
2024-03-10 07:33:10 +0200

    add custom scroll view  to book details

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -12,64 +12,68 @@ class BookDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            const CustomBookDetailsAppBar(),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.38,
-              child: const CustomBookItem(),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Text(
-              "The Jungle Book",
-              style: Styles.textStyle32,
-            ),
-            Text(
-              "Rudyard Kipling",
-              style: Styles.textStyle20.copyWith(
-                color: Colors.grey,
-                fontStyle: FontStyle.italic,
-              ),
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            const BookRating(
-              mainAxisAlignment: MainAxisAlignment.center,
-            ),
-            const SizedBox(
-              height: 32,
-            ),
-            const BookAction(),
-            const SizedBox(
-              height: 40,
-            ),
-            const Padding(
-              padding: EdgeInsets.only(left: 16.0),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "You can also like",
-                  style: Styles.textStyle18,
+      body: CustomScrollView(
+        slivers: [
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Column(
+              children: [
+                const CustomBookDetailsAppBar(),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.38,
+                  child: const CustomBookItem(),
                 ),
-        
-              ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text(
+                  "The Jungle Book",
+                  style: Styles.textStyle32,
+                ),
+                Text(
+                  "Rudyard Kipling",
+                  style: Styles.textStyle20.copyWith(
+                    color: Colors.grey,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                const BookRating(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                ),
+                const SizedBox(
+                  height: 32,
+                ),
+                const BookAction(),
+                const Expanded(
+                  child: SizedBox(
+                    height: 50,
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 16.0),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "You can also like",
+                      style: Styles.textStyle18,
+                    ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 16.0),
+                  child: SimilarBooksListView(),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+              ],
             ),
-            const Padding(
-              padding: EdgeInsets.only(left: 16.0),
-              child: SimilarBooksListView(),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
 }
-
-
