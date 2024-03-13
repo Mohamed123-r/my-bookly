063d025231504ec9249cac9fdb99b67c0ad64767
mohamed <mezoattalla123@gmail.com>
2024-03-13 14:23:00 +0200

    create news books cubit method

--- lib/features/home/presentation/manager/news_books/news_books_cubit.dart
+++ lib/features/home/presentation/manager/news_books/news_books_cubit.dart
@@ -2,10 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:my_bookly/features/home/data/repos/home_repo.dart';
 import '../../../data/models/BookModel.dart';
-
 part 'news_books_state.dart';
 
 class NewsBooksCubit extends Cubit<NewsBooksState> {
-  NewsBooksCubit() : super(NewsBooksInitial());
+  NewsBooksCubit(this.homeRepo) : super(NewsBooksInitial());
+  final HomeRepo homeRepo;
 
+  Future<void> fetchFeatureBooks() async {
+    emit(NewsBooksLoading());
+
+    var result = await homeRepo.fetchFeaturedBooks();
+
+    result.fold(
+          (failure) {
+        emit(
+          NewsBooksFailure(errorMessage: failure.errorMessage),
+        );
+      },
+          (books) {
+        emit(
+          NewsBooksSuccess(books: books),
+        );
+      },
+    );
+  }
 }
