5559729352547a59a0b22a5e49f68d84e4d5f988
mohamed <mezoattalla123@gmail.com>
2024-03-20 09:35:07 +0200

    Updated custom_best_seller_item.dart to include a GestureDetector for tapping, triggering navigation to Book Details View.

--- lib/features/home/presentation/views/widgets/custom_best_seller_item.dart
+++ lib/features/home/presentation/views/widgets/custom_best_seller_item.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:my_bookly/core/utils/styles.dart';
 import 'package:my_bookly/features/home/presentation/views/widgets/custom_book_item.dart';
 
+import '../../../../../core/utils/app_router.dart';
 import '../../../data/models/BookModel.dart';
 import 'book_rating.dart';
 
@@ -13,58 +15,65 @@ class BestSellerListViewItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: SizedBox(
-        height: 135,
-        child: Row(
-          children: [
-            CustomBookItem(
-              urlImage: book.volumeInfo.imageLinks?.thumbnail?? "",
-            ),
-            const SizedBox(
-              width: 24,
-            ),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * .6,
-                    child: Text(
-                      book.volumeInfo.title!,
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      style: Styles.textStyle24,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 3,
-                  ),
-                  Text(
-                    book.volumeInfo.authors![0],
-                    style: Styles.textStyle16.copyWith(color: Colors.grey),
-                  ),
-                  const SizedBox(
-                    height: 3,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      const Text(
-                        "Free",
-                        style: Styles.textStyle20,
-                      ),
-                      BookRating(
-                        rating: book.volumeInfo.averageRating ?? 0,
-                        ratingCount: book.volumeInfo.ratingsCount ?? 0,
-                      ),
-                    ],
-                  )
-                ],
+      child: GestureDetector(
+        onTap: () {
+           GoRouter.of(context).push(
+            AppRouter.kBookDetailsView,
+           );
+        },
+        child: SizedBox(
+          height: 135,
+          child: Row(
+            children: [
+              CustomBookItem(
+                urlImage: book.volumeInfo.imageLinks?.thumbnail?? "",
               ),
-            ),
-          ],
+              const SizedBox(
+                width: 24,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * .6,
+                      child: Text(
+                        book.volumeInfo.title!,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: Styles.textStyle24,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 3,
+                    ),
+                    Text(
+                      book.volumeInfo.authors![0],
+                      style: Styles.textStyle16.copyWith(color: Colors.grey),
+                    ),
+                    const SizedBox(
+                      height: 3,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        const Text(
+                          "Free",
+                          style: Styles.textStyle20,
+                        ),
+                        BookRating(
+                          rating: book.volumeInfo.averageRating ?? 0,
+                          ratingCount: book.volumeInfo.ratingsCount ?? 0,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
