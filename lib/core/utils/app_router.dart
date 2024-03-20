56ac7e52a48c83fbf297462d33dbc565048b12b3
mohamed <mezoattalla123@gmail.com>
2024-03-20 10:32:40 +0200

    provide similar Cubit

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:my_bookly/core/utils/service_locator.dart';
+import 'package:my_bookly/features/home/data/repos/home_repo_impl.dart';
+import 'package:my_bookly/features/home/presentation/manager/similar_book_cubit/similar_book_cubit.dart';
 import 'package:my_bookly/features/home/presentation/views/book_details_view.dart';
 import 'package:my_bookly/features/search/presentation/views/search_view.dart';
 import 'package:my_bookly/features/splash/presentation/views/splash_view.dart';
@@ -26,7 +30,10 @@ abstract class AppRouter {
       GoRoute(
         path: kBookDetailsView,
         builder: (BuildContext context, GoRouterState state) {
-          return const BookDetailsView();
+          return BlocProvider(
+              create: (BuildContext context) =>
+                  SimilarBookCubit(getIt.get<HomeRepoImpl>()),
+              child: const BookDetailsView());
         },
       ),
       GoRoute(
